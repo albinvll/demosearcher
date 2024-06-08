ed22cb34c3786bb465351066bf9e9fb454374530
Laiho <emillaiho@hotmail.fi>
2024-06-08 20:38:19 +0300

    add 1vX

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ document.getElementById('searchForm').addEventListener('submit', function(e) {
     e.preventDefault();
     var startTime = performance.now();
     const weaponName = document.getElementById('weapon').value;
-    const nRoundKills = document.getElementById('kills').value;
+    const roundType = document.getElementById('roundType').value;
     const files = document.getElementById('demoFiles').files;
     let totalFiles = files.length;
     let totalSize = 0;
     let processedFiles = 0;
     document.getElementById('fileCounter').textContent = 'Demos searched: ' + processedFiles + '/' + totalFiles;
-
+    
     // Some web worker stuff
     const numWorkers = Math.min(DEFAULT_N_WORKERS, files.length);
     const workers = [];
@@ -24,7 +24,7 @@ document.getElementById('searchForm').addEventListener('submit', function(e) {
     let fileIdx = 0;
     for (let i = 0; i < numWorkers; i++){
         totalSize += tasks[fileIdx].size;
-        workers[i].postMessage({ file: tasks[i] });
+        workers[i].postMessage({ file: tasks[fileIdx], "weaponName": weaponName,  "roundType": roundType});
         fileIdx++;
     }
 
@@ -34,67 +34,43 @@ document.getElementById('searchForm').addEventListener('submit', function(e) {
         }
         worker.onmessage = function (e) {
           processedFiles++;
-          generateTableFromData(e.data.json, weaponName, nRoundKills, e.data.file);
+          generateTableFromData(e.data.output);
           document.getElementById('fileCounter').textContent = 'Demos searched: ' + processedFiles + '/' + totalFiles;
           
           // process next demo if any left
           if (fileIdx < totalFiles){
             totalSize += tasks[fileIdx].size;
-            worker.postMessage({ file: tasks[fileIdx] });
+            worker.postMessage({ file: tasks[fileIdx], "weaponName": weaponName,  "roundType": roundType});
+
             fileIdx++;
+          }else{
+            // Check if we are done
+            if (processedFiles == totalFiles){
+                console.log("DONE")
+                var endTime = performance.now();
+                document.getElementById('fileCounter').textContent = 'Done in ' + ((endTime - startTime) / 1000).toFixed(2) + 
+                ' seconds' + ' (' + (totalSize / 1000000000).toFixed(2) + ' GB)';
+                // terminate all workers after we are done.
+                workers.forEach((w, index) => {
+                    w.terminate();
+                });
+                return;
+            }
           }
-          // Check if we are done
-          if (processedFiles == totalFiles){
-            var endTime = performance.now();
-            document.getElementById('fileCounter').textContent = 'Done in ' + ((endTime - startTime) / 1000).toFixed(2) + 
-            ' seconds' + ' (' + (totalSize / 1000000000).toFixed(2) + ' GB)';
-            // terminate all workers after we are done.
-            workers.forEach((w, index) => {
-                w.terminate();
-            });
-            return;
-          }
+
         };
     });
+    
 });
 
-function generateTableFromData(eventsNotFiltered, weaponName, nRoundKills, fileName) {
-    let events = eventsNotFiltered.filter(e => e.get("is_warmup_period") == false);
-    let maxRound = Math.max(...events.map(event => event.get("total_rounds_played")));
-    const wantedRows = [];
-    for (let round = 0; round <= maxRound; round++) {
-        const killsPerPlayer = {};
-        let killsThisRound = events.filter(kill => kill.get("total_rounds_played") == round);
+function generateTableFromData(output) {
 
-        killsThisRound.forEach(item => {
-            const attackerName = item.get("attacker_name");
-            const kills = killsPerPlayer[attackerName] || 0;
-            if (item.get("weapon") === weaponName || weaponName == "Any" ||
-             (weaponName == "knife" && item.get("weapon").includes("knife"))||
-             (weaponName == "knife" && item.get("weapon") == "bayonet") ) {
-                killsPerPlayer[attackerName] = kills + 1;
-            }
-        });
-
-        for (let [key, value] of Object.entries(killsPerPlayer)) {
-            if (value == nRoundKills) {
-                let wantedKills = events.filter(kill => kill.get("total_rounds_played") == round && kill.get("attacker_name") == key &&
-                (kill.get("weapon") === weaponName || weaponName == "Any" ||
-                (weaponName == "knife" && kill.get("weapon").includes("knife"))||
-                (weaponName == "knife" && kill.get("weapon") == "bayonet") ));
-                if (wantedKills.length > 0){
-                    wantedRows.push({"name": key, "kills": value, "round": round + 1, "file": fileName, "tick": wantedKills[0].get("tick")});
-                }
-            }
-        }
-    }
     let table = document.getElementById("table").querySelector("tbody");
-    wantedRows.forEach((rowItem, i) => {
+    output.forEach((rowItem, i) => {
         let row = table.insertRow();
         row.insertCell(0).innerHTML = rowItem.name;
-        row.insertCell(1).innerHTML = rowItem.kills;
-        row.insertCell(2).innerHTML = rowItem.round;
-        row.insertCell(3).innerHTML = rowItem.tick;
-        row.insertCell(4).innerHTML = rowItem.file;
+        row.insertCell(1).innerHTML = rowItem.round;
+        row.insertCell(2).innerHTML = rowItem.tick;
+        row.insertCell(3).innerHTML = rowItem.file;
     });
 }
--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 importScripts('./pkg/demoparser2.js');
 
-const { parseEvent} = wasm_bindgen;
+const { parseEvent, parseEvents, parseTicks} = wasm_bindgen;
 
 async function loadWorker() {
     await wasm_bindgen('./pkg/demoparser2_bg.wasm');
@@ -9,12 +9,146 @@ async function loadWorker() {
 loadWorker();
 
 onmessage = async function(event) {
+    let roundType = event.data.roundType;
+
+    if (roundType.includes("v")){
+        return parse1vX(event);
+    }else{
+        return parseKills(event);
+    }
+};
+
+
+function parseKills(event) {
     let fileName = event.data.file.name;
+    const weaponName = event.data.weaponName;
+    const nRoundKills = parseInt(event.data.roundType[0]);
+
     const reader = new FileReader();
     reader.onload = function (event) {
         const uint8Array = new Uint8Array(event.target.result);
         const result = parseEvent(uint8Array, "player_death", ["health"], ["total_rounds_played", "is_warmup_period"]);
-        postMessage({"json": result, "file": fileName});
+
+        let events = result.filter(e => e.get("is_warmup_period") == false);
+        let maxRound = Math.max(...events.map(event => event.get("total_rounds_played")));
+        const wantedRows = [];
+        for (let round = 0; round <= maxRound; round++) {
+            const killsPerPlayer = {};
+            let killsThisRound = events.filter(kill => kill.get("total_rounds_played") == round);
+        
+            killsThisRound.forEach(item => {
+                const attackerName = item.get("attacker_name");
+                const kills = killsPerPlayer[attackerName] || 0;
+                if (item.get("weapon") === weaponName || weaponName == "Any" ||
+                 (weaponName == "knife" && item.get("weapon").includes("knife"))||
+                 (weaponName == "knife" && item.get("weapon") == "bayonet") ) {
+                    killsPerPlayer[attackerName] = kills + 1;
+                }
+            });
+        
+            for (let [key, value] of Object.entries(killsPerPlayer)) {
+                if (value == nRoundKills) {
+                    let wantedKills = events.filter(kill => kill.get("total_rounds_played") == round && kill.get("attacker_name") == key &&
+                    (kill.get("weapon") === weaponName || weaponName == "Any" ||
+                    (weaponName == "knife" && kill.get("weapon").includes("knife"))||
+                    (weaponName == "knife" && kill.get("weapon") == "bayonet") ));
+
+                    if (wantedKills.length > 0){
+                        wantedRows.push({"name": key, "round": round + 1, "file": fileName, "tick": wantedKills[0].get("tick")});
+                    }
+                }
+            }
+        }
+        postMessage({"output": wantedRows});
+    };
+    reader.readAsArrayBuffer(event.data.file);
+};
+
+
+
+function parse1vX(event) {
+    let fileName = event.data.file.name;
+    let X = event.data.roundType[2];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const uint8Array = new Uint8Array(event.target.result);
+
+        let output = [];
+
+        let event_json = parseEvents(uint8Array, ["player_death", "round_end", "player_spawn", "round_freeze_end"], ["X", "Y", "team_name"], ["total_rounds_played"])
+        let roundEnds = event_json.filter(x => x.get("event_name") == "round_end");
+        let roundEndArr = [];
+        for (const end of roundEnds){
+            roundEndArr[end.get("total_rounds_played")] = end;
+        }
+        
+        var Talive = new Set();
+        var CTalive = new Set();
+        var currentRound = 0;
+        var roundHasStarted = false;
+        var bestOneVx = [];
+        
+        for (const e of event_json){
+            if (roundHasStarted){
+                if (Talive.size == 1){
+                    bestOneVx.push({"team": "T", "X": CTalive.size, "clutcher": Array.from(Talive)[0], "round": e.get("total_rounds_played"), "tick": e.get("tick")});
+                }
+                if (CTalive.size == 1){
+                    bestOneVx.push({"team": "CT", "X": Talive.size, "clutcher": Array.from(CTalive)[0], "round": e.get("total_rounds_played"), "tick": e.get("tick")});
+                }
+            }
+            // Spawn
+            if (e.get("event_name") == "player_spawn"){
+                if (e.get("user_team_name") == "CT"){
+                    CTalive.add(e.get("user_name"));
+                }else if (e.get("user_team_name") == "TERRORIST"){
+                    Talive.add(e.get("user_name"));
+                }
+            }
+            // Death
+            if (e.get("event_name") == "player_death"){
+                if (e.get("user_team_name") == "CT"){
+                    CTalive.delete(e.get("user_name"));
+                }else if (e.get("user_team_name") == "TERRORIST"){
+                    Talive.delete(e.get("user_name"));
+                }
+            }
+            // Round end
+            if (e.get("event_name") == "round_end"){
+                if (bestOneVx.length > 0){
+    
+                    // legacy stuff
+                    let winnerString = "";
+                    if (e.get("winner") == 3 || e.get("winner") == "CT"){
+                        winnerString = "CT";
+                    }else if (e.get("winner") == 2 || e.get("winner") == "T"){
+                        winnerString = "T";
+                    }
+    
+                    // If player also won the round (killing everyone is not enough/correct)
+                    if (bestOneVx[0].team == winnerString){
+                        // Are all 1v4 also 1v3? One could argue that a ninja defuse in a 1v4 does never go into a 1v3 state
+                        // but for now we assume this to be true
+                        if (bestOneVx[0].X >= X){
+
+                            output.push({"name": bestOneVx[0].clutcher, "round": bestOneVx[0].round + 1, "file": fileName, "tick": bestOneVx[0].tick});
+                            console.log(bestOneVx[0], fileName);
+                        }
+                    }
+                }
+                bestOneVx = [];
+                Talive = new Set();
+                CTalive = new Set();
+                currentRound++;
+                roundHasStarted = false;
+            }
+            // Round start
+            if (e.get("event_name") == "round_freeze_end"){
+                roundHasStarted = true;
+                currentRound = e.get("total_rounds_played");
+            }
+        }
+        postMessage({"output": output});
     };
     reader.readAsArrayBuffer(event.data.file);
 };
