690fe46ca4cb054d4a128d213e7cb225cdcff7fc
albinvll <albin.vllahiu@gmail.com>
2024-08-18 17:36:46 +0200

    dewebbing complete

--- index.js
+++ index.js
@@ -1,77 +1,4 @@
-const DEFAULT_N_WORKERS = 8;
-
-const processDemo = (weaponName, roundType, files) => {
-	var startTime = performance.now();
-	let totalFiles = files.length;
-	let totalSize = 0;
-	let processedFiles = 0;
-	document.getElementById("fileCounter").textContent =
-		"Demos searched: " + processedFiles + "/" + totalFiles;
-
-	// Some web worker stuff
-	const numWorkers = Math.min(DEFAULT_N_WORKERS, files.length);
-	const workers = [];
-	for (let i = 0; i < numWorkers; i++) {
-		workers.push(new Worker("./worker.js"));
-	}
-	let tasks = [];
-	for (let i = 0; i < files.length; i++) {
-		tasks.push(files[i]);
-	}
-	let fileIdx = 0;
-	for (let i = 0; i < numWorkers; i++) {
-		totalSize += tasks[fileIdx].size;
-		workers[i].postMessage({
-			file: tasks[fileIdx],
-			weaponName: weaponName,
-			roundType: roundType,
-		});
-		fileIdx++;
-	}
-
-	workers.forEach((worker, index) => {
-		worker.onerror = function (e) {
-			processedFiles++;
-		};
-		worker.onmessage = function (e) {
-			processedFiles++;
-			generateTableFromData(e.data.output);
-			const responseText =
-				"Demos searched: " + processedFiles + "/" + totalFiles;
-			sendMessage(responseText);
-
-			// process next demo if any left
-			if (fileIdx < totalFiles) {
-				totalSize += tasks[fileIdx].size;
-				worker.postMessage({
-					file: tasks[fileIdx],
-					weaponName: weaponName,
-					roundType: roundType,
-				});
-
-				fileIdx++;
-			} else {
-				// Check if we are done
-				if (processedFiles == totalFiles) {
-					console.log("DONE");
-					var endTime = performance.now();
-					const responseText =
-						"Done in " +
-						((endTime - startTime) / 1000).toFixed(2) +
-						" seconds" +
-						" (" +
-						(totalSize / 1000000000).toFixed(2) +
-						" GB)";
-					sendMessage(responseText);
-					workers.forEach((w, index) => {
-						w.terminate();
-					});
-					return;
-				}
-			}
-		};
-	});
-};
+import { processDemo } from "./processing";
 const sendMessage = (responseText) => {
 	document.getElementById("fileCounter").textContent = responseText;
 };
@@ -83,12 +10,18 @@ const main = () => {
 	const weaponName = document.getElementById("weapon").value;
 	const roundType = document.getElementById("roundType").value;
 	const files = document.getElementById("demoFiles").files;
-	processDemo(weaponName, roundType, files);
+	processDemo(
+		weaponName,
+		roundType,
+		files,
+		sendMessage,
+		generateTableFromData
+	);
 };
 
 document.getElementById("searchForm").addEventListener("submit", main);
 
-function generateDataArrayFromData(output) {
+function generateDataArrayFromData(output, showData) {
 	let dataArray = [];
 
 	output.forEach((rowItem) => {
@@ -101,7 +34,7 @@ function generateDataArrayFromData(output) {
 		};
 		dataArray.push(dataObject);
 	});
-
+	showData(dataArray);
 	return dataArray;
 }
 
---
+++ processing.js
@@ -0,0 +1,97 @@
+const DEFAULT_N_WORKERS = 8;
+
+export const processDemo = (
+	weaponName,
+	roundType,
+	files,
+	sendMessage,
+	showResult
+) => {
+	var startTime = performance.now();
+	let totalFiles = files.length;
+	let totalSize = 0;
+	let processedFiles = 0;
+	document.getElementById("fileCounter").textContent =
+		"Demos searched: " + processedFiles + "/" + totalFiles;
+
+	// Some web worker stuff
+	const numWorkers = Math.min(DEFAULT_N_WORKERS, files.length);
+	const workers = [];
+	for (let i = 0; i < numWorkers; i++) {
+		workers.push(new Worker("./worker.js"));
+	}
+	let tasks = [];
+	for (let i = 0; i < files.length; i++) {
+		tasks.push(files[i]);
+	}
+	let fileIdx = 0;
+	for (let i = 0; i < numWorkers; i++) {
+		totalSize += tasks[fileIdx].size;
+		workers[i].postMessage({
+			file: tasks[fileIdx],
+			weaponName: weaponName,
+			roundType: roundType,
+		});
+		fileIdx++;
+	}
+
+	workers.forEach((worker, index) => {
+		worker.onerror = function (e) {
+			processedFiles++;
+		};
+		worker.onmessage = function (e) {
+			processedFiles++;
+			generateDataArrayFromData(e.data.output, showResult);
+			const responseText =
+				"Demos searched: " + processedFiles + "/" + totalFiles;
+			sendMessage(responseText);
+
+			// process next demo if any left
+			if (fileIdx < totalFiles) {
+				totalSize += tasks[fileIdx].size;
+				worker.postMessage({
+					file: tasks[fileIdx],
+					weaponName: weaponName,
+					roundType: roundType,
+				});
+
+				fileIdx++;
+			} else {
+				// Check if we are done
+				if (processedFiles == totalFiles) {
+					console.log("DONE");
+					var endTime = performance.now();
+					const responseText =
+						"Done in " +
+						((endTime - startTime) / 1000).toFixed(2) +
+						" seconds" +
+						" (" +
+						(totalSize / 1000000000).toFixed(2) +
+						" GB)";
+					sendMessage(responseText);
+					workers.forEach((w, index) => {
+						w.terminate();
+					});
+					return;
+				}
+			}
+		};
+	});
+};
+
+function generateDataArrayFromData(output, showData) {
+	let dataArray = [];
+
+	output.forEach((rowItem) => {
+		// Create an object for each row and push it into the dataArray
+		let dataObject = {
+			name: rowItem.name,
+			round: rowItem.round,
+			tick: rowItem.tick,
+			file: rowItem.file,
+		};
+		dataArray.push(dataObject);
+	});
+	showData(dataArray);
+	return dataArray;
+}
